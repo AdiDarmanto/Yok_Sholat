79929dbd74c5343eb57bdab41e28aee4ed49a0cb
AdiDarmanto <68941647+AdiDarmanto@users.noreply.github.com>
2022-03-28 22:11:11 +0700

    Imported Unity Project
    compressing AR2.FBX to .zip file, must be decompressed before used

---
+++ Assets/AnimasiTombol.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimasiTombol : MonoBehaviour
+{
+    public void _Animasi()
+        {
+        GetComponent<Animation>().Play("New Animation");
+        KumpulanSuara.instance.Panggil_Suara(0);
+        }
+        
+}
